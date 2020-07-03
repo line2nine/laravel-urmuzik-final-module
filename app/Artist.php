92ec27a9663f3e31bcb9f6adfed6242ba0ddd8d8
tienanh <tienanh12108991@gmail.com>
2020-07-03 15:03:00 +0700

    up new songs updated

--- app/Artist.php
+++ app/Artist.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Artist extends Model
 {
     protected $table = 'artists';
+
+    function songs() {
+        return $this->hasMany('App\Song','artist_id');
+    }
 }
--- app/Song.php
+++ app/Song.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Song extends Model
 {
     protected $table = 'songs';
+
+    function artist() {
+        return $this->belongsTo('App\Artist','artist_id');
+    }
 }

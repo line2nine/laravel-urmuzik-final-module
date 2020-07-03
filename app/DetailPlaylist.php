55e8fa78f5e4951c12e53a80fef70c989c26de86
daovanduc <duc07092000@gmail.com>
2020-07-03 16:46:57 +0700

    connect detail add song and playlist

--- app/DetailPlaylist.php
+++ app/DetailPlaylist.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class DetailPlaylist extends Model
 {
     protected $table = 'detail_playlists';
+
+    function song()
+    {
+        return $this->belongsTo('App\Song', 'song_id');
+    }
+
+    function playlist()
+    {
+        return $this->belongsTo('App\Playlist', 'playlist_id');
+    }
 }

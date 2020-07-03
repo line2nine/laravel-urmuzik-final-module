a6a754893d42bf4fe01132a293850ad0766ebbdb
line2nine <hailinh203@gmail.com>
2020-07-03 15:03:39 +0700

    Fix syntax error

--- routes/web.php
+++ routes/web.php
@@ -24,25 +24,27 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('register', 'UserController@create')->name('register');
 Route::post('register', 'UserController@store');
 
-Route::group(['prefix'=>'user'],function (){
-    Route::middleware(['auth'])->group(function (){
-        Route::get('upload','Music\SongController@create')->name('music.upload');  // co van de gi do o day
-        Route::post('upload','Music\SongController@store')->name('music.store'); // van de nay la lung
-        Route::get('{id}/list-song-user','Music\SongController@listSongUser')->name('music.list.user');
+Route::group(['prefix' => 'user'], function () {
+    Route::middleware(['auth'])->group(function () {
+        Route::get('upload', 'Music\SongController@create')->name('music.upload');  // co van de gi do o day
+        Route::post('upload', 'Music\SongController@store')->name('music.store'); // van de nay la lung
+        Route::get('{id}/list-song-user', 'Music\SongController@listSongUser')->name('music.list.user');
         Route::get('{id}/edit-song', 'Music\SongController@edit')->name('music.edit');
         Route::post('{id}/edit-song', 'Music\SongController@update')->name('music.update');
         Route::get('{id}/delete-song', 'Music\SongController@destroy')->name('music.delete');
         Route::post('playlist/add', 'Home\PlaylistController@create')->name('playlist.add');
+    });
+});
 
 Route::group(['prefix' => 'playlist'], function () {
-   Route::get('/', 'Home\PlaylistController@index')->name('playlist.index');
+    Route::get('/', 'Home\PlaylistController@index')->name('playlist.index');
 });
 
 Route::group(['prefix' => 'songs'], function () {
     Route::get('/', 'Music\SongController@index')->name('music.index');
     Route::get('/{id}', 'Music\SongController@show')->name('music.play');
 });
-  
+
 Route::middleware(['auth', 'check.role'])->group(function () {
     Route::group(['prefix' => 'admin'], function () {
         Route::get('dashboard', 'UserController@index')->name('admin.dashboard');
@@ -59,4 +61,4 @@ Route::middleware(['auth', 'check.role'])->group(function () {
             Route::post('{id}/change-password', 'UserController@updatePass');
         });
     });
-});  
+});

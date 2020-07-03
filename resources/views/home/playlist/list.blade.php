f930ab62691be8c78e128ad81bb2ae508f0e0c01
daovanduc <duc07092000@gmail.com>
2020-07-03 11:21:53 +0700

    update list playlist

--- resources/views/home/playlist/list.blade.php
+++ resources/views/home/playlist/list.blade.php
@@ -6,7 +6,7 @@
             <h2>My playlist</h2>
         </div>
     </section>
-@include('home.playlist.add')
+
     <!-- ##### Album Catagory Area Start ##### -->
     <section class="album-catagory section-padding-100-0">
         <div class="container">
@@ -48,161 +48,19 @@
             <div class="row oneMusic-albums">
 
                 <!-- Single Album -->
-                <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item t c p">
-                    <div class="single-album">
-                        <img src="img/bg-img/a1.jpg" alt="">
-                        <div class="album-info">
-                            <a href="#">
-                                <h5>The Cure</h5>
-                            </a>
-                            <p>Second Song</p>
+                @foreach($playlists as $key => $playlist)
+                    <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item t c p">
+                        <div class="single-album">
+                            <img src="img/bg-img/a{{random_int(1,12)}}.jpg" alt="">
+                            <div class="album-info">
+                                <a href="#">
+                                    <h5>{{$playlist->title}}</h5>
+                                </a>
+                                <p>{{$playlist->user->name}}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                <!-- Single Album -->
-                <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item s e q">
-                    <div class="single-album">
-                        <img src="img/bg-img/a2.jpg" alt="">
-                        <div class="album-info">
-                            <a href="#">
-                                <h5>Sam Smith</h5>
-                            </a>
-                            <p>Underground</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Album -->
-                <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item w f r">
-                    <div class="single-album">
-                        <img src="img/bg-img/a3.jpg" alt="">
-                        <div class="album-info">
-                            <a href="#">
-                                <h5>Will I am</h5>
-                            </a>
-                            <p>First</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Album -->
-                <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item t g u">
-                    <div class="single-album">
-                        <img src="img/bg-img/a4.jpg" alt="">
-                        <div class="album-info">
-                            <a href="#">
-                                <h5>The Cure</h5>
-                            </a>
-                            <p>Second Song</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Album -->
-                <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item d h v">
-                    <div class="single-album">
-                        <img src="img/bg-img/a5.jpg" alt="">
-                        <div class="album-info">
-                            <a href="#">
-                                <h5>DJ SMITH</h5>
-                            </a>
-                            <p>The Album</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Album -->
-                <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item t i x">
-                    <div class="single-album">
-                        <img src="img/bg-img/a6.jpg" alt="">
-                        <div class="album-info">
-                            <a href="#">
-                                <h5>The Ustopable</h5>
-                            </a>
-                            <p>Unplugged</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Album -->
-                <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item b j y">
-                    <div class="single-album">
-                        <img src="img/bg-img/a7.jpg" alt="">
-                        <div class="album-info">
-                            <a href="#">
-                                <h5>Beyonce</h5>
-                            </a>
-                            <p>Songs</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Album -->
-                <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item a k z">
-                    <div class="single-album">
-                        <img src="img/bg-img/a8.jpg" alt="">
-                        <div class="album-info">
-                            <a href="#">
-                                <h5>Aam Smith</h5>
-                            </a>
-                            <p>Underground</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Album -->
-                <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item w l number">
-                    <div class="single-album">
-                        <img src="img/bg-img/a9.jpg" alt="">
-                        <div class="album-info">
-                            <a href="#">
-                                <h5>Will I am</h5>
-                            </a>
-                            <p>First</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Album -->
-                <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item d m">
-                    <div class="single-album">
-                        <img src="img/bg-img/a10.jpg" alt="">
-                        <div class="album-info">
-                            <a href="#">
-                                <h5>DJ SMITH</h5>
-                            </a>
-                            <p>The Album</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Album -->
-                <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item t n">
-                    <div class="single-album">
-                        <img src="img/bg-img/a11.jpg" alt="">
-                        <div class="album-info">
-                            <a href="#">
-                                <h5>The Ustopable</h5>
-                            </a>
-                            <p>Unplugged</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Single Album -->
-                <div class="col-12 col-sm-4 col-md-3 col-lg-2 single-album-item b o">
-                    <div class="single-album">
-                        <img src="img/bg-img/a12.jpg" alt="">
-                        <div class="album-info">
-                            <a href="#">
-                                <h5>Beyonce</h5>
-                            </a>
-                            <p>Songs</p>
-                        </div>
-                    </div>
-                </div>
-
+                @endforeach
             </div>
         </div>
     </section>

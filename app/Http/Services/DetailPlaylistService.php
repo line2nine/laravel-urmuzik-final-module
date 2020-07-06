a5ca68022a8b746779fc9c1317c5c8d73b4a2956
daovanduc <duc07092000@gmail.com>
2020-07-06 10:10:26 +0700

    add songs playlist

--- app/Http/Services/DetailPlaylistService.php
+++ app/Http/Services/DetailPlaylistService.php
@@ -4,7 +4,9 @@
 namespace App\Http\Services;
 
 
+use App\DetailPlaylist;
 use App\Http\Repositories\DetailPlaylistRepository;
+use Illuminate\Support\Facades\Auth;
 
 class DetailPlaylistService
 {
@@ -18,6 +20,20 @@ class DetailPlaylistService
     public function getSongByPlaylistId($playlist_id)
     {
         return $this->detailPlaylistRepository->getSongByPlaylistId($playlist_id);
+    }
+
+    public function addSongPlaylist($request, $playlist_id)
+    {
+        $count = $request->count;
+        for ($i = 0; $i < $count; $i++) {
+            $requestSongId = $request->input('song' . $i);
+            if ($requestSongId !== null) {
+                $detailPlaylist = new DetailPlaylist();
+                $detailPlaylist->song_id = $requestSongId;
+                $detailPlaylist->playlist_id = $playlist_id;
+                $this->detailPlaylistRepository->save($detailPlaylist);
+            }
+        }
     }
 
 }

038c87267cefe2344dbebb41c1dddf829a645661
daovanduc <duc07092000@gmail.com>
2020-07-07 10:23:41 +0700

    view edit playlist modal

---
+++ resources/views/home/playlist/edit.blade.php
@@ -0,0 +1,35 @@
+<!-- Modal -->
+<div class="modal fade" id="editPlaylist" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form method="post" action="{{route('my-playlist.update', ['playlist_id' => $playlist->id])}}">
+                @csrf
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Edit Playlist</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Name playlist</label>
+                        <input type="text" class="form-control
+                        @if($errors->first('title'))
+                            is-invalid
+                        @endif
+                            " value="{{$playlist->title}}" name="title" required minlength="2" maxlength="15">
+                        @if($errors->first('title'))
+                            <p class="text-danger">{{ $errors->first('title') }}</p>
+                        @endif
+
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>

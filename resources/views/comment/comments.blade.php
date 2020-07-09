ec0d7d39fe1568033d6e3d90ff021bfbb30ede37
daovanduc <duc07092000@gmail.com>
2020-07-09 14:04:47 +0700

    form new comment

---
+++ resources/views/comment/comments.blade.php
@@ -0,0 +1,11 @@
+<div class="col-md-12">
+    <form method="post" action="{{route('comments.song', ['song_id' => $song->id])}}">
+        @csrf
+        <div class="form-group" style="color: white">
+            <textarea class="form-control" rows="2" name="comment"></textarea>
+        </div>
+        <div class="form-group text-right">
+            <button class="btn btn-primary" type="submit">Post</button>
+        </div>
+    </form>
+</div>

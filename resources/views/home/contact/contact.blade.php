d90de94c9d9ea94b6604069357d00cf07bcd8638
line2nine <hailinh203@gmail.com>
2020-07-13 11:57:18 +0700

    Create contact page

---
+++ resources/views/home/contact/contact.blade.php
@@ -0,0 +1,71 @@
+@extends('home.master')
+@section('content')
+    <!-- ##### Breadcumb Area Start ##### -->
+    <section class="breadcumb-area bg-img bg-overlay" style="background-image: url({{asset('img/bg-img/breadcumb.jpg')}});">
+        <div class="bradcumbContent">
+            <h2>Contact</h2>
+        </div>
+    </section>
+    <!-- ##### Breadcumb Area End ##### -->
+
+    <!-- ##### Contact Area Start ##### -->
+    <section class="contact-area section-padding-100-0">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-12 col-lg-3">
+                    <div class="contact-content mb-100">
+                        <!-- Title -->
+                        <div class="contact-title mb-50">
+                            <h5>Contact Info</h5>
+                        </div>
+
+                        <!-- Single Contact Info -->
+                        <div class="single-contact-info d-flex align-items-center">
+                            <div class="icon mr-30">
+                                <span class="icon-placeholder"></span>
+                            </div>
+                            <p>23, TT-01, MonCity – Hàm Nghi Street, Mỹ Đình 1, Nam Từ Liêm, Hà Nội</p>
+                        </div>
+
+                        <!-- Single Contact Info -->
+                        <div class="single-contact-info d-flex align-items-center">
+                            <div class="icon mr-30">
+                                <span class="icon-smartphone"></span>
+                            </div>
+                            <p>+84 123 456 789</p>
+                        </div>
+
+                        <!-- Single Contact Info -->
+                        <div class="single-contact-info d-flex align-items-center">
+                            <div class="icon mr-30">
+                                <span class="icon-mail"></span>
+                            </div>
+                            <p>support@urmuzik.com</p>
+                        </div>
+
+                        <!-- Contact Social Info -->
+                        <div class="contact-social-info mt-50">
+                            <a href="#" data-toggle="tooltip" data-placement="top" title="Pinterest"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="top" title="Dribbble"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="top" title="Behance"><i class="fa fa-behance" aria-hidden="true"></i></a>
+                            <a href="#" data-toggle="tooltip" data-placement="top" title="Linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-12 col-lg-9">
+                    <!-- ##### Google Maps ##### -->
+                    <div class="map-area mb-100">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.926563248193!2d105.7661268143082!3d21.03562419292115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b9444c676b%3A0xe713200541b7456d!2sCodeGym!5e0!3m2!1svi!2s!4v1594614491655!5m2!1svi!2s" width="600" height="450" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- ##### Contact Area End ##### -->
+@endsection

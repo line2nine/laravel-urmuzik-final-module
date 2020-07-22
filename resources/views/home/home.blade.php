e673cb82b8c4ee13d172b3ed223b0d03377151b9
longnguyen69 <nguyenthanhlongtlck@gmail.com>
2020-07-22 09:19:51 +0700

    edit new song on home page

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -54,16 +54,15 @@
                 <div class="col-12 col-lg-4">
                     <div class="new-hits-area mb-100">
                         <div class="section-heading text-left mb-50 wow fadeInUp" data-wow-delay="50ms">
-                            <h2>Top Likes</h2>
+                            <h2>New Song</h2>
                         </div>
 
                         <!-- Single Top Item -->
-                        @foreach($songLike as $song)
+                        @foreach($songs as $song)
                             <div class="single-new-item d-flex align-items-center justify-content-between wow fadeInUp"
                                  data-wow-delay="100ms">
                                 <div class="first-part d-flex align-items-center">
                                     <div class="thumbnail">
-{{--                                        <img src="{{asset('storage/' . $like->song->image)}}" style="width: 70px; height: 70px" alt="image">--}}
                                         <img src="{{asset('storage/' . $song->image)}}" style="width: 70px; height: 70px" alt="image">
                                     </div>
                                     <div class="content-">
@@ -136,37 +135,6 @@
         </div>
     </section>
     <!-- ##### Miscellaneous Area End ##### -->
-
-    <!-- ##### Latest Albums Area Start ##### -->
-    <section class="latest-albums-area section-padding-100">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section-heading style-2">
-                        <h2>New Songs</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12">
-                    <div class="albums-slideshow owl-carousel">
-                        <!-- Single Album -->
-                        @foreach($songs as $song)
-                        <div class="single-album">
-                            <img src="{{asset('storage/' . $song->image)}}" alt="" style="width: 190px; height: 190px">
-                            <div class="album-info">
-                                <a href="{{ route('music.play',['id'=>$song->id]) }}">
-                                    <h5>{{$song->artist->name}}</h5>
-                                    <p>{{ $song->name }}</p>
-                                </a>
-                            </div>
-                        </div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
     <!-- ##### Latest Albums Area End ##### -->
     <section class="latest-albums-area section-padding-100">
         <div class="container">
